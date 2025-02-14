a3c970462500e758016c09867204dd2c9fd0e8b3
speransky_d <dsppernakiy@gmail.com>
2025-02-14 12:22:18 +0300

    Новая версия + пример входных данных

--- main_v2.py
+++ main_v2.py
@@ -1,9 +1,7 @@
 import requests
-import re
 import json
 from bs4 import BeautifulSoup
 import pandas as pd
-import time
 import os
 
 data = pd.read_excel(r"data.xlsx")
@@ -13,9 +11,9 @@ results = []
 for row in rows:
     # URL и заголовки запроса
     url = f"https://www.detmir.ru/search/results/?qt={row}&searchType=auto&searchMode=common"
+    # url = f"https://www.detmir.ru/product/index/id/{row}/"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 YaBrowser/24.7.0.0 Safari/537.36",
-        "Cookie": "your_cookie_here"  # Замените на ваши куки, если это необходимо
     }
 
     try:
@@ -26,8 +24,8 @@ for row in rows:
         if response.status_code == 200:
             # Используем BeautifulSoup для обработки HTML
             soup = BeautifulSoup(response.text, 'html.parser')
-
             # Поиск всех <script> тегов
+
             script_datas = soup.find_all("script")
             for script_data in script_datas:
                 if script_data.string and 'appData' in script_data.string:
@@ -39,12 +37,10 @@ for row in rows:
                         json_text = json_text.replace(r'\\"', '')
                         json_text = json_text.replace(r'\"', '"')  # Убираем экранирование
                         json_text = json_text[1:]
-
                         app_data = json.loads(json_text)  # Преобразуем в словарь Python
                         break  # Завершаем после первого совпадения
                     except Exception as e:
                         print("Ошибка при обработке JSON:", e)
-
             # Поиск нужного товара по SKU и добавление данных в результаты
             for suggestion in app_data['search']['data']['suggestions']:
                 if suggestion['type'] == 'product':
@@ -61,8 +57,12 @@ for row in rows:
     except Exception as e:
         print(f"Ошибка при обработке SKU {row}: {e}")
 
-if results:
-    output_df = pd.DataFrame(results)
-    output_df.to_excel('results.xlsx', index=False)
-else:
-    print("Нет данных для записи.")
+print(results)
+
+if not os.path.exists(r"results.xlsx"):
+    with open(r"results.xlsx", "w") as file:
+        if results:
+            output_df = pd.DataFrame(results)
+            output_df.to_excel(file, index=False)
+        else:
+            print("Нет данных для записи.")
